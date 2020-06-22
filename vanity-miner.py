e429d90abc27b3a7e2fed2fa875f29d61c35689a
staffano <s0565913@htw-berlin.de>
2020-06-22 02:38:15 +0200

    added metrics

--- vanity-miner.py
+++ vanity-miner.py
@@ -4,10 +4,28 @@ import bitcoin
 import time
 import sys
 import os
+from humanfriendly import format_timespan
 
 event = mp.Event()
 search = '1'
 processors = 1
+
+
+class Counter(object):
+    def __init__(self):
+        self.val = mp.Value('i', 0)
+
+    def increment(self, n=1):
+        with self.val.get_lock():
+            self.val.value += n
+
+    @property
+    def value(self):
+        return self.val.value
+
+
+counter = Counter()
+counter.__init__()
 
 
 def check_cpu_count(cpu_count: int):
@@ -49,17 +67,27 @@ def bitcoin_address(secret_key):
 
 
 def search_address():
-    tries = 0
     while True:
         if event.is_set():
-            return tries
+            return
 
         secret = random_secret()
         address, pubkey = bitcoin_address(secret)
-        tries += 1
-        if search_addr in address[0:len(search_addr)]:
+        counter.increment()
+        hashing_power = counter.value / (time.time() - start)
+        estimated_time = int((frequency - counter.value) / hashing_power)
+
+        if estimated_time < 0:
+            estimated_time = 0
+        if counter.value % 100 == 0:
+            print(int((counter.value / frequency) * 100), "%", "| Tries to freq :", (counter.value, frequency),
+                  "| Hashing power: %.2f h/s" % hashing_power,
+                  "| Average remaining time: ", format_timespan(estimated_time),
+                  end='\r')
+
+        if search_addr.lower() in address[0:len(search_addr)].lower():
             event.set()
-            return secret, pubkey, address, tries
+            return secret, pubkey, address
 
 
 if __name__ == "__main__":
@@ -67,6 +95,7 @@ if __name__ == "__main__":
     start = time.time()
     search_addr, processors = check_sys_arg()
     base58_check(search_addr)
+    frequency = 58 ** (len(search_addr) - 1)
     pool = mp.Pool(processors)
     results = []
 
@@ -77,31 +106,25 @@ if __name__ == "__main__":
         result = pool.apply_async(search_address)
         results.append(result)
     pool.close()
-    event.wait()
 
     solution = []
-    amount = []
+
     for i in results:
-        if not (isinstance(i.get(), int)):
+        if i.get() is not None:
             solution = i.get()
-            amount.append(i.get()[3])
-        else:
-            amount.append(i.get())
 
     pool.terminate()
 
-    assert (len(amount) == processors)
     assert (bitcoin.is_privkey(solution[0]))
     assert (bitcoin.is_pubkey(solution[1]))
     assert (bitcoin.is_address(solution[2]))
 
-    print("Private key in HEX :", bitcoin.encode_privkey(solution[0], 'hex'), os.linesep + "Public key in HEX :",
+    print("\nPrivate key in HEX :", bitcoin.encode_privkey(solution[0], 'hex'), os.linesep + "Public key in HEX :",
           bitcoin.encode_pubkey(solution[1], 'hex'),
           os.linesep + "Address :", solution[2])
 
     end = time.time()
-    hashes = sum(amount)
     runtime = float(end - start)
     print(os.linesep + "Elapsed Time: %.4fs" % runtime)
-    print("Amount of tries : %d" % hashes)
-    print("Hash power :  %.4f h/s" % (hashes / runtime))
+    print("Amount of tries : %d" % counter.value)
+    print("Hash power :  %.4f h/s" % (counter.value / runtime))
