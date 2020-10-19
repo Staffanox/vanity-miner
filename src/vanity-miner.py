0ce0331c10fec22aac1b1585a9ad54b5c6b6887c
staffano <s0565913@htw-berlin.de>
2020-10-19 14:16:06 +0200

    added file output

--- src/vanity-miner.py
+++ src/vanity-miner.py
@@ -19,7 +19,7 @@ counter.__init__()
 
 def check_cpu_count(cpu_count: int):
     if cpu_count > os.cpu_count():
-        return 1
+        return os.cpu_count()
 
     return int(cpu_count)
 
@@ -69,14 +69,21 @@ def search_address():
         counter.increment()
         hashing_power = counter.value / (time.time() - start)
         estimated_time = int((frequency - counter.value) / hashing_power)
+        elapsed_time = time.time() - start
 
-        if estimated_time < 0:
-            estimated_time = 0
-        if counter.value % (processors * 20) == 0:
-            print(int((counter.value / frequency) * 100), "%", "| Tries to freq :", (counter.value, frequency),
-                  "| Hashing power: %.2f h/s" % hashing_power,
-                  "| Average remaining time: %s" % format_timespan(estimated_time),
-                  end='\r')
+        if counter.value % (processors * 100) == 0:
+            if estimated_time <= 0:
+                print("\r", int((counter.value / frequency) * 100), "%", "| Tries to freq :",
+                      (counter.value, frequency),
+                      "| Hashing power: %.2f h/s" % hashing_power,
+                      "| Elapsed time: %s" % format_timespan(elapsed_time),
+                      end='\r', flush=True)
+            else:
+                print("\r", int((counter.value / frequency) * 100), "%", "| Tries to freq :",
+                      (counter.value, frequency),
+                      "| Hashing power: %.2f h/s" % hashing_power,
+                      "| Average remaining time: %s" % format_timespan(estimated_time),
+                      end='\r', flush=True)
 
         if case_sensitivity is True:
             if search_addr in address[0:len(search_addr)]:
@@ -86,7 +93,6 @@ def search_address():
             if search_addr.lower() in address[0:len(search_addr)].lower():
                 event.set()
                 return secret, pubkey, address
-
 
 
 if __name__ == "__main__":
@@ -126,8 +132,24 @@ if __name__ == "__main__":
           bitcoin.encode_pubkey(solution[1], 'hex'),
           os.linesep + "Address :", solution[2])
 
-    end = time.time()
-    runtime = float(end - start)
+    runtime = float(time.time() - start)
+    hash_rate = counter.value / runtime
     print(os.linesep + "Elapsed Time: %.4fs" % runtime)
     print("Amount of tries : %d" % counter.value)
-    print("Hash power :  %.4f h/s" % (counter.value / runtime))
+    print("Hash power :  %.4f h/s" % hash_rate)
+
+    if "-d" in sys.argv:
+        with open("address.txt", 'w') as address:
+            address.write("Private key in HEX : " + bitcoin.encode_privkey(solution[0], 'hex') + os.linesep)
+            address.write("Public key in HEX : " +
+                          bitcoin.encode_pubkey(solution[1], 'hex') + os.linesep)
+            address.write("Address : " + solution[2] + os.linesep)
+
+        address.close()
+
+        with open("metadata.txt", 'w') as meta_data:
+            meta_data.write(
+                "Used " + str(processors) + " out of " + str(os.cpu_count()) + " available processors" + os.linesep)
+            meta_data.write("Elapsed time : " + str(format_timespan(runtime)) + os.linesep)
+            meta_data.write("Hash power during mining was : %.4f h/s" % hash_rate + os.linesep)
+        meta_data.close()
